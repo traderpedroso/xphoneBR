ca974965e500c18b090b1ea5e0fdf89f242679e3
Emerson Pedroso <emerson@bitcoinnano.org>
2024-06-21 11:13:00 -0300

    fix monetary

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ be accurate, fast, easy to use.
 DeepPhonemizerBR is compatible with Python 3.6+ and is distributed under the MIT license.
 
 """
-# Version: 0.0.5
+# Version: 0.0.6
 setup(
     name="xphonebr",
-    version="0.0.5",
+    version="0.0.6",
     author="Emerson Pedroso",
     author_email="traderpedroso@icloud.com",
     description="Grapheme to phoneme conversion and tools for tts with deep learning.",
